af3496f943bd0403a008785d8858fb029d3bb1ef
Aixi Wang <aixi.wang@hotmail.com>
2016-02-14 22:09:32 +0800

    Create global_setting.py

---
+++ global_setting.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#---------------------------------------
+#
+# Copyright(c) Aixi Wang 2014-2015
+#---------------------------------------
+# v1 -- initial version
+#---------------------------------------
+
+#-----------------------
+# mail
+#-----------------------
+global mail_sender,mail_smtpserver,mail_username,mail_password
+global mail_enable,mail_to
+
+mail_to = 'xx@xxx.xxx'
+mail_username = 'xxx@xxx.xxx'
+mail_password = 'xxx'
+mail_smtpserver = 'xxx.xxx.xxx'
+mail_sender = 'xxx@xxx.xxx'
+mail_enable = 1
